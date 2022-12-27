d73e99d10fc9c57fc39b7c1250ca7fafb67b29a4
Moses Fawole <mosesfawole2001@gmail.com>
2022-12-27 20:53:09 +0100

    crews page mobile design complete

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -8,7 +8,7 @@ const Crew = () => {
     <div>
       <section>
         <div className="flex flex-col gap-8 ">
-          <div className="top flex flex-col justify-center items-center gap-8">
+          <div className="top flex flex-col justify-center items-center gap-8 relative">
             <div className="title font-barlow text-white flex gap-4 ">
               <p className="font-bold tracking-[2.7px] opacity-[0.25]">02</p>
               <p className="sm:text-[20px] sm:leading-6  uppercase tracking-[2.7px]">
@@ -16,9 +16,9 @@ const Crew = () => {
               </p>
             </div>
             <div className="">
-              <img className="w-1/2 mx-auto" src={images.webp} alt={name} />
-              <div className="h-[1px] w-4/5 mx-auto bg-[#383B4B]"></div>
+              <img className="w-full h-[222px]" src={images.webp} alt={name} />
             </div>
+            <div className="absolute bottom-0 h-[1px] w-[327px] mx-auto bg-[#383B4B]"></div>
           </div>
 
           <div className="bottom">
@@ -34,14 +34,16 @@ const Crew = () => {
                 </li>
               ))}
             </ul>
-            <div className="details text-center mt-8">
+            <div className="details text-center mt-8 px-6">
               <div className="role">
-                <p className="font-bellefair  text-base leading-4 opacity-[0.5]">
+                <p className="font-bellefair text-white  text-base uppercase leading-4 opacity-[0.5]">
                   {role}
                 </p>
               </div>
               <div className="name mt-2 mb-4">
-                <p className="text-2xl leading-7 text-white ">{name}</p>
+                <p className="text-2xl leading-7 text-white uppercase    ">
+                  {name}
+                </p>
               </div>
               <div className="bio">
                 <p className="font-barlow text-[15px] leading-6 text-[#D0D6F9]">

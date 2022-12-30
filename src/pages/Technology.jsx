660d36faa9da985d6ead9d3b7c5aec6406da0c29
Moses Fawole <mosesfawole2001@gmail.com>
2022-12-30 08:23:45 +0100

    initail technology page

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "../components/Header";
 import Tech from "../components/Tech";
 
 const Technology = () => {
   return (
-    <div>
+    <div className="bg-mobileTech sm:bg-tabletTech md:bg-desktopTech bg-cover bg-center min-h-screen">
+      <Header />
       <Tech />
     </div>
   );

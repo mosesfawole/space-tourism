09b66e844d7a45ba31c6a68495ebd89d44aaf96a
Moses Fawole <mosesfawole2001@gmail.com>
2022-12-30 08:19:39 +0100

    crewsp age completed

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -7,9 +7,9 @@ const Crew = () => {
   return (
     <div>
       <section>
-        <div className="flex flex-col md:flex-row items-center md:gap-0  gap-8 ">
-          <div className="top flex flex-col justify-center items-center gap-8 relative order-last">
-            <div className="md:hidden title font-barlow text-white flex gap-4 ">
+        <div className="flex flex-col md:flex-row justify-center items-center md:gap-20  gap-8 ">
+          <div className="top flex flex-col justify-center items-center gap-8 relative md:basis-2/6 md:order-last">
+            <div className=" md:hidden title font-barlow text-white flex gap-4 ">
               <p className="font-bold tracking-[2.7px] opacity-[0.25]">02</p>
               <p className="sm:text-[20px] sm:leading-6  uppercase tracking-[2.7px] ">
                 Meet your crew
@@ -25,8 +25,8 @@ const Crew = () => {
             <div className="md:hidden absolute bottom-0 h-[1px] w-[327px] mx-auto bg-[#383B4B]"></div>
           </div>
 
-          <div className="bottom flex flex-col ">
-            <div className="hidden md:flex title font-barlow text-white gap-4 ">
+          <div className="bottom flex flex-col md:basis-[45%] ">
+            <div className="hidden md:mb-28 md:flex title font-barlow text-white gap-4 md:pl-8 ">
               <p className="font-bold tracking-[2.7px] opacity-[0.25]">02</p>
               <p className="sm:text-[20px] sm:leading-6  uppercase tracking-[2.7px] ">
                 Meet your crew
@@ -55,7 +55,7 @@ const Crew = () => {
                   {name}
                 </p>
               </div>
-              <div className="bio">
+              <div className="bio md:mb-20">
                 <p className="font-barlow text-[15px] leading-6 text-[#D0D6F9] md:text-[18px] md:leading-8">
                   {" "}
                   {bio}

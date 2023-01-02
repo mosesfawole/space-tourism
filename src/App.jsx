f7821d8d7916806058c785b113806409912320ef
Moses Fawole <mosesfawole2001@gmail.com>
2023-01-02 11:23:51 +0100

    remove the last few lines of code lol

--- src/App.jsx
+++ src/App.jsx
@@ -7,38 +7,8 @@ import Technology from "./pages/Technology";
 import { useEffect, useState } from "react";
 
 const App = (props) => {
-  const location = useLocation();
-
-  const [currentPage, setCurrentPage] = useState("/");
-
-  useEffect(() => {
-    if (location.pathname === "/" || location.pathname === "home") {
-      setCurrentPage("home");
-    }
-    if (location.pathname === "destination") {
-      setCurrentPage("destination");
-    }
-    if (location.pathname === "crew") {
-      setCurrentPage("crew");
-    }
-    if (location.pathname === "technology") {
-      setCurrentPage("technology");
-    }
-  });
   return (
-    <div
-      className={
-        currentPage === "home"
-          ? "bg-mobile sm:bg-tablet md:bg-desktop bg-center bg-cover min-h-screen"
-          : currentPage === "destination"
-          ? "bg-mobileDestination sm:bg-tabletDestination md:bg-desktopDestination bg-center bg-cover min-h-screen -screen"
-          : currentPage === "crew"
-          ? "bg-mobileCrew sm:bg-tabletCrew md:bg-desktopCrew  bg-cover bg-center min-h-screen"
-          : currentPage === "technology"
-          ? "bg-mobileTech sm:bg-tabletTech md:bg-desktopTech bg-cover bg-center min-h-screen"
-          : ""
-      }
-    >
+    <div>
       <Header />
       <Routes>
         {/* <Route path="*" element={} /> */}

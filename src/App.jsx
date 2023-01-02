fe21ac745d05112775adc2694e1bcc7f2e058fa7
Moses Fawole <mosesfawole2001@gmail.com>
2023-01-02 11:37:55 +0100

    restore removed lines and delete new lines in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,38 @@ import Technology from "./pages/Technology";
 import { useEffect, useState } from "react";
 
 const App = (props) => {
+  const { pathname } = useLocation();
+
+  const [currentPage, setCurrentPage] = useState("/");
+
+  useEffect(() => {
+    if (pathname === "/" || pathname === "/home") {
+      setCurrentPage("home");
+    }
+    if (pathname === "/destination") {
+      setCurrentPage("destination");
+    }
+    if (pathname === "/crew") {
+      setCurrentPage("crew");
+    }
+    if (pathname === "/technology") {
+      setCurrentPage("technology");
+    }
+  }, [pathname]);
   return (
-    <div>
+    <div
+      className={
+        currentPage === "home"
+          ? "bg-mobile sm:bg-tablet md:bg-desktop bg-center bg-cover min-h-screen"
+          : currentPage === "destination"
+          ? "bg-mobileDestination sm:bg-tabletDestination md:bg-desktopDestination bg-center bg-cover min-h-screen -screen"
+          : currentPage === "crew"
+          ? "bg-mobileCrew sm:bg-tabletCrew md:bg-desktopCrew  bg-cover bg-center min-h-screen"
+          : currentPage === "technology"
+          ? "bg-mobileTech sm:bg-tabletTech md:bg-desktopTech bg-cover bg-center min-h-screen"
+          : ""
+      }
+    >
       <Header />
       <Routes>
         {/* <Route path="*" element={} /> */}

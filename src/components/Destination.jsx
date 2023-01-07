5e59eb3eea8658e6b33ce55a21cdc42be3fbb7c3
Moses Fawole <mosesfawole2001@gmail.com>
2023-01-07 14:32:06 +0100

    fit destination page

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -18,7 +18,7 @@ const Destination = () => {
             </div>
             <img
               loading="lazy"
-              className="w-1/2 sm:w-1/2 md:w-auto  md:mt-7 mx-auto"
+              className="w-1/2 sm:w-1/2 md:w-1/3  md:mt-7 mx-auto"
               src={images.webp}
               alt={name}
             />
